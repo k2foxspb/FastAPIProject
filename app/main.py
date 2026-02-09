c7f40265f9a010ec7c75dfc6885017b8829068c8
k2fox <k2foxspb@mail.ru>
2026-02-09 12:41:58 +0300

    middleware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
+import time
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.requests import Request
 
 from .routers import reviews, users, categories, products, cart, orders, payments
 
@@ -13,6 +21,48 @@ app_v1 = FastAPI(
     version="0.2.0",
 )
 
+
+class TimingMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        start_time = time.time()
+        await self.app(scope, receive, send)
+        duration = time.time() - start_time
+        print(f"Request duration: {duration:.10f} seconds")
+
+
+# мидлвар на основе функции
+# @app.middleware("http")
+# async def modify_request_response_middleware(request: Request, call_next):
+#     start_time = time.time()
+#     response = await call_next(request)
+#     duration = time.time() - start_time
+#     print(f"Request duration: {duration:.10f} seconds")
+#     return response
+app.add_middleware(TimingMiddleware)
+
+origins = [
+    "http://127.0.0.1:8000",
+    "http://localhost:63342",
+    "https://example.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["Authorization", "Content-Type"],
+)
+app.add_middleware(
+    TrustedHostMiddleware, allowed_hosts=["127.0.0.1", "localhost:8000", "*"]
+)
+# app.add_middleware(HTTPSRedirectMiddleware)
+
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+app.add_middleware(SessionMiddleware, secret_key="7UzGQS7woBazLUtVQJG39ywOP7J7lkPkB0UmDhMgBR8=")
 app.include_router(categories.router)
 app.include_router(products.router)
 app.include_router(users.router)
@@ -21,6 +71,7 @@ app.include_router(cart.router)
 app.include_router(orders.router)
 app.include_router(payments.router)
 
+
 # Корневой эндпоинт для проверки
 @app.get("/")
 async def root():
@@ -28,6 +79,24 @@ async def root():
     Корневой маршрут, подтверждающий, что API работает.
     """
     return {"message": "Добро пожаловать в API интернет-магазина!"}
+
+
+@app.get("/create_session")
+async def session_set(request: Request):
+    request.session["my_session"] = "1234"
+    return 'ok'
+
+
+@app.get("/read_session")
+async def session_info(request: Request):
+    my_var = request.session.get("my_session")
+    return my_var
+
+
+@app.get("/delete_session")
+async def session_delete(request: Request):
+    my_var = request.session.pop("my_session")
+    return my_var
 
 
 app.mount("/media", StaticFiles(directory="media"), name="media")
