81ae85ad5e4b4033ea5a22a361f1b2529eb05890
k2fox <k2foxspb@mail.ru>
2026-02-12 14:35:14 +0300

    docker

--- app/api/routers/health.py
+++ app/api/routers/health.py
@@ -1,15 +1,20 @@
 from datetime import timezone, timedelta, datetime
+from typing import List
 
 from fastapi import APIRouter, BackgroundTasks, Request
 from loguru import logger
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+from starlette.websockets import WebSocketDisconnect, WebSocket
 
 from app.celery.celery_app import celery
 from app.celery.tasks import call_background_task
 
+
 router = APIRouter(prefix="", tags=["health"])
 
 
-@router.get("/")
+@router.get("/1")
 async def root():
     """Корневой маршрут, подтверждающий, что API работает."""
     logger.info("Root path accessed")
@@ -62,3 +67,38 @@ async def test_celery_task():
     logger.info("Celery task dispatched")
 
     return {"message": "Task sent to Celery"}
+templates = Jinja2Templates(directory="app/templates")
+@router.get("/", response_class=HTMLResponse)
+def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.connections: List[WebSocket] = []
+        print("Creating a list to active connections", self.connections)
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.connections.append(websocket)
+        print("New Active connections are ", self.connections)
+
+    async def broadcast(self, data: str):
+        for connection in self.connections:
+            await connection.send_text(data)
+            print("In broadcast: sent msg to ", connection)
+
+manager = ConnectionManager()
+
+@router.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"Client {client_id}: {data}")
+    except WebSocketDisconnect as e:
+        manager.connections.remove(websocket)
+        print(f'Connection closed {e.code}')
