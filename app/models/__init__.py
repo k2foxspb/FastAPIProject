e2e6b9813bc94a1f7f68bf052d63971f9ba96f76
k2fox <k2foxspb@mail.ru>
2026-02-21 14:12:27 +0300

    1

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,12 +4,12 @@ from .orders import Order, OrderItem
 from .news import News, NewsImage
 from .products import Product, ProductImage
 from .reviews import Reviews
-from .users import User, UserPhoto, AdminPermission, AppVersion
+from .users import User, UserPhoto, AdminPermission, AppVersion, PhotoAlbum, Friendship
 from .chat import ChatMessage, FileUploadSession
 
 
 __all__ = ["Category", "Product", "ProductImage", "News", "NewsImage",
-           'User', 'UserPhoto', 'AdminPermission', 'AppVersion',
+           'User', 'UserPhoto', 'AdminPermission', 'AppVersion', 'PhotoAlbum', 'Friendship',
            'Reviews','CartItem',
            "Order", "OrderItem",
            "ChatMessage", "FileUploadSession"
